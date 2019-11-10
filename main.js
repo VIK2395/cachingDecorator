af7ebb252023f01ea74b60535ad6bfe5a4e1241a
VIK2395 <50545334+VIK2395@users.noreply.github.com>
2019-11-10 21:40:19 +0200

    update

--- main.js
+++ main.js
@@ -1,43 +1,15 @@
 ﻿'use strict'
 
-// function slow(x) {
-//   // здесь могут быть ресурсоёмкие вычисления
-//   alert(`Called with ${x}`);
-//   return x;
-// }
-
-// function cachingDecorator(func) {
-//   let cache = new Map();
-
-//   return function(x) {
-//     if (cache.has(x)) {    // если кеш содержит такой x,
-//       return cache.get(x); // читаем из него результат
-//     }
-
-//     let result = func(x); // иначе, вызываем функцию
-
-//     cache.set(x, result); // и кешируем (запоминаем) результат
-//     return result;
-//   };
-// }
-
-// slow = cachingDecorator(slow);
-
-// alert( slow(1) ); // slow(1) кешируем
-// alert( "Again: " + slow(1) ); // возвращаем из кеша
-
-// alert( slow(2) ); // slow(2) кешируем
-// alert( "Again: " + slow(2) ); // возвращаем из кеша
-
-
 function cachingDecorator(func) {
 	let cache = new Map();// {name => age}
 
 	return function(name, age) {
+		if (name === 'show cache') {console.log(cache); return;}
+
 		if ( cache.has(name) ) {
 			if ( cache.get(name) === age ) {
 				console.log("Hello from cache!"); return cache.get(name) 
-			} else map.delete(name);
+			} else cache.delete(name);
 		} 
 
 
@@ -55,9 +27,14 @@ function getUserAge(name, age) {
 
 getUserAge = cachingDecorator(getUserAge);
 
-alert( getUserAge('Viktor', 24) ); //кешуэм
-alert( "Again: " + getUserAge('Viktor', 24) ); //Берем з кеша
+getUserAge('Viktor', 24);
+getUserAge('Mufasa', 14);
 
-alert( getUserAge('Mufasa', 14) ); //кешуэм
+getUserAge('show cache');
 
-alert( "Again: " + getUserAge('Mufasa', 14) );
+getUserAge('Viktor', 27);
+
+getUserAge('show cache');
+
+
+
